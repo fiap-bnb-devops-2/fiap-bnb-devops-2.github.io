b1b263e523aa5bddec618f9b4b0fbb5a45aa3ec5
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-01 10:04:17 -0300

    feat: test 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aplicação React do BNB
+          Aplicação React do BNB - Teste 1
         </p>
         <a
           className="App-link"
