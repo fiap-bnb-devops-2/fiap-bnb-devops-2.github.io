dd0146ab972ad3e097bc9e4be4aff3bf483dc93c
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-01 11:49:43 -0300

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testando deploy automático no GitHub Pages
+          Deploy automático no GitHub Pages usando um worflow personalizado
         </p>
         <a
           className="App-link"
