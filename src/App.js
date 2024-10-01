26f4cef106a94c7277b8043d8d51ab619016e81a
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-01 09:40:47 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './bnb.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicação React do BNB
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hcode.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda DevOps
         </a>
       </header>
     </div>
