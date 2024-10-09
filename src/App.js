66545fbb7403197ac6eeb700b9fe0ba7633e292e
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-09 09:36:43 -0300

    netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploy automático no GitHub Pages usando um worflow personalizado
+          Deploy automático na Netlify!!!
         </p>
 
         <a
