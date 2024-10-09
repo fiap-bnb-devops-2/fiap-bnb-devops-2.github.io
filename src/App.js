c75d90f0ba7eb7afb31a2b1646f1858755e56bc8
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-09 09:39:13 -0300

    deploy github pages and netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deploy automático na Netlify!!!
+          Deploy automático na Netlify e no GitHub Pages!!!
         </p>
 
         <a
