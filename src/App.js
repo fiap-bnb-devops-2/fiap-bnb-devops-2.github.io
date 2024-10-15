29f37e32249f0829757e6c883f563c2027e16de8
Rafael Ribeiro <anthonyribeiro1910@gmail.com>
2024-10-15 10:30:07 -0300

    feat: changing main text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testando a atualização na AWS
+          Testando a atualização na AWS por meio do GitHub Actions
         </p>
 
         <a
